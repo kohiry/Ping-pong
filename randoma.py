8920ec0949395f0fc2cdec5892c56f6397124d6e
Kohiry <kohiry@mail.ru>
2020-01-15 10:58:31 -0800

    добавил уже работающию прорисовк, но не до конца разобрался с классом

--- randoma.py
+++ randoma.py
@@ -6,11 +6,21 @@ class obj:
     def __init__(self, x):
         self.x = x
         self.y = 300
-        self.speed = 10
+        self.speed = 1
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        self.clock.tick(350)
+        if self.x > 0:
+            self.x -= self.speed
+        else:
+            self.x = 602
+        del self
+
 
     def draw(self, screen):
-        pygame.draw.rect(screen, White, (x - 25, y - 25, 50, 50))
-        self.x -= self.speed
+        pygame.draw.rect(screen, White, (self.x - 25, self.y - 25, 50, 50))
+        self.run()
 
 
 size = width, height = 600, 600
@@ -25,36 +35,35 @@ width_inside_obj = 100
 rects = []
 
 def randoma(x):
-    x = x
-    print(x)
     n = random.randint(1, 3)
-    if n == 3:
-        width_inside_obj = 150
-    elif n == 2:
-        width_inside_obj = 250
-    else:
-        if random.randint(1, 2) == 2:
-            width_inside_obj = 0
-        else:
-            width_inside_obj = 100
-
-    for i in range(n):
+    if n == 1:
         rects.append(obj(x))
-        x += width_inside_obj
-    return x
+    elif n == 2:
+        for i in range(n):
+            rects.append(obj(x))
+            x += 300
+    else:
+        for i in range(n):
+            rects.append(obj(x))
+            x += 200
+
+
 
 flag = True
 running = True
-clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            rects.clear()
+            randoma(x)
 
     screen.fill(Black)
     if flag:
         randoma(x)
         flag = False
+        print(rects)
     for i in rects:
         i.draw(screen)
     pygame.display.flip()
