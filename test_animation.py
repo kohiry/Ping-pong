7270183f4dea04bfae56d7595daf05a6eda10fef
Kohiry <kohiry@mail.ru>
2020-01-15 17:12:50 -0800

    add my sprite и cod написанный мною не в гите во избежание ошибок

--- test_animation.py
+++ test_animation.py
@@ -1,1 +1,47 @@
-# тут будет тест с проработкой анимации
+import pygame
+import os
+pygame.init()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+
+size = width, height = 800, 310
+screen = pygame.display.set_mode(size)
+
+# создадим группу, содержащую все спрайты
+all_sprites = pygame.sprite.Group()
+
+def addsprite(art):
+    print(1)
+    # создадим спрайт
+    sprite = pygame.sprite.Sprite()
+    # определим его вид
+    sprite.image = pygame.image.load(f'data/{art}.bmp')
+    # и размеры
+    sprite.rect = sprite.image.get_rect(bottomright=(50, 70))
+    # добавим спрайт в группу
+    all_sprites.add(sprite)
+    sprite.rect.x = 50
+    sprite.rect.y = 240
+
+
+
+image_surf = pygame.image.load('data/background1.bmp')
+image_rect = image_surf.get_rect(bottomright=(800, 310))
+screen.blit(image_surf, image_rect)
+addsprite('hero')
+addsprite('hero_run_1')
+all_sprites.draw(screen)
+pygame.display.update()
+
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
