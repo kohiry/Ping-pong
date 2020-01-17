0d298df5070e54069b838a9de8774bcf37bfc6b9
Kohiry <kohiry@mail.ru>
2020-01-17 09:45:25 -0800

    not all

--- help.py
+++ help.py
@@ -1,3 +1,7 @@
+import pygame
+import random
+
+
 class obj:
     def __init__(self, x):
         self.x = x
@@ -17,3 +21,34 @@ class obj:
         pygame.draw.rect(screen, White, (self.x - 25, self.y - 25, 50, 50))
         if self.run():
             return True
+
+def random_for_move_obstacles(x):
+    n = random.randint(2, 5)
+    if n == 2:
+        for i in range(n):
+            rects.append(obj(x))
+            x += 300
+    elif n == 3:
+        for i in range(n):
+            rects.append(obj(x))
+            x += 200
+    elif n == 4:
+        for i in range(2):
+            rects.append(obj(x))
+            x += 50
+    elif n == 5:
+        for i in range(3):
+            rects.append(obj(x))
+            x += 50
+
+def download():
+    return {'die_sound': pygame.mixer.Sound(r'Sound\die.ogg'),
+            'jump_sound': pygame.mixer.Sound(r'Sound\jump.ogg'),
+            'score_sound': pygame.mixer.Sound(r'Sound\score.ogg')}
+
+def hero_sprite():
+    return {'move':[pygame.image.load(f'data/hero_run_1.png'),
+            pygame.image.load(f'data/hero_run_2.png')],
+            'jump':pygame.image.load(f'data/hero.png'),
+            'die':pygame.image.load(f'data/hero_die.png'),
+            'score':pygame.image.load(f'data/hero_get_score.png'),               'secret move': pygame.image.load(f'data/hero_move_eyes.png')}
