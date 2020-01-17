2ff2d733e8be2f4ea9f6524bfbb29c75aa25e766
Kohiry <kohiry@mail.ru>
2020-01-17 11:12:01 -0800

    start work

---
+++ game.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+import help
+pygame.init()
+
+
+size = width, height = 600, 600
+screen = pygame.display.set_mode(size)
+
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pass
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pass
+            elif event.key == pygame.K_DOWN:
+                pass
+    screen.fill(Black)
+    pygame.display.flip()
+
+pygame.quit()
