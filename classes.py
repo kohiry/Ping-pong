0f39e5cdfb2173e21405641d78b517d751994356
Kohiry <kohiry@mail.ru>
2020-01-21 11:31:26 -0800

    game 0.0.2 (работающий класс, создаются шарики способные отражаться от стен)

---
+++ classes.py
@@ -0,0 +1,52 @@
+import pygame
+pygame.init()
+
+size = width, height = 800, 600
+
+class Ball:
+    def __init__(self, coord, screen):
+        self.pos = coord
+        self.x, self.y = -100, -100
+        self.screen = screen
+        self.clock = pygame.time.Clock()
+
+    def run(self, x, y, coord, width, height):
+      speed = (x, y)
+      speeds_all = [(-100, -100), (100, 100), (100, -100), (-100, 100)]
+      if speed == speeds_all[0]:
+        if coord[0] <= 0:
+          return (100, -100)
+        if coord[1] <= 0:
+          return (-100, 100)
+        else:
+          return speed
+      elif speed == speeds_all[1]:
+        if coord[0] >= width:
+          return (-100, 100)
+        if coord[1] >= height:
+          return (100, -100)
+        else:
+          return speed
+      elif speed == speeds_all[2]:
+        if coord[0] >= width:
+          return (-100, -100)
+        if coord[1] <= 0:
+          return (100, 100)
+        else:
+          return speed
+      elif speed == speeds_all[3]:
+        if coord[0] <= 0:
+          return (100, 100)
+        if coord[1] >= height:
+          return (-100, -100)
+        else:
+          return speed
+
+    def draw(self, width, height):
+        x, y = self.pos
+        self.x, self.y = self.run(self.x, self.y, self.pos, width, height)
+        x += self.x / 80
+        y += self.y / 80
+        self.clock.tick(80)
+        self.pos = (int(x), int(y))
+        pygame.draw.circle(self.screen, (255, 255, 255), self.pos, 10)
--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 import pygame
+import classes
 pygame.init()
 
 
-size = width, height = 600, 600
+size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 
 White = (255, 255, 255)
 Black = (0, 0, 0)
+list_balls = []
+bol = False
 
 running = True
 clock = pygame.time.Clock()
@@ -15,22 +18,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            pass
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                pass
-            elif event.key == pygame.K_DOWN:
-                pass
+            list_balls.append(classes.Ball(event.pos, screen))
+            bol = True
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         pass
     elif keys[pygame.K_RIGHT]:
         pass
-    elif keys[pygame.K_A]:
+    elif keys[pygame.K_a]:
         pass
-    elif keys[pygame.K_D]:
+    elif keys[pygame.K_d]:
         pass
     screen.fill(Black)
+    if bol:
+        for i in list_balls:
+            i.draw(width, height)
     pygame.display.flip()
 
 pygame.quit()
